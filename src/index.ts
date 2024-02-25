1cf10a2cb85d5b0619fdade7c28a8206c20bc075
Luis Vilar <luis_velazquez@estudante.sesisenai.org.br>
2024-02-25 19:59:40 -0300

    Actualizar configuración de compilación

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,16 @@
-console.log('Hello, world!')
+// Loguer para Express
 
+import { RequestHandler, Request, Response, NextFunction } from "express";
+import fs from "fs/promises";
+
+function logger(enableOutput: Boolean = true): RequestHandler {
+  return async (req: Request, res: Response, next: NextFunction) => {
+    const date = new Date();
+    const log = `[ ${req.method} ]${req.originalUrl} ${date} [ IP ]${req.ip}\n\n`;
+    console.log(log);
+    enableOutput && (await fs.appendFile("log.txt", log));
+    next();
+  };
+}
+export default logger;
+module.exports = logger;
