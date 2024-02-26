27bdd9e26860e39f1edc0aa1b07c847aa3d39dc5
Luis Vilar <luis_velazquez@estudante.sesisenai.org.br>
2024-02-25 23:47:36 -0300

    Añadir función de salvar en un JSON el log

--- src/index.ts
+++ src/index.ts
@@ -3,13 +3,54 @@
 import { RequestHandler, Request, Response, NextFunction } from "express";
 import fs from "fs/promises";
 
+/**
+ * Loguer para Express
+ * @param enableOutput - Si se habilita, se guardará un archivo log.json con la información de las peticiones
+ * @returns RequestHandler
+ */
+
 function logger(enableOutput: Boolean): RequestHandler {
   return (req: Request, res: Response, next: NextFunction) => {
     const date = new Date();
     res.on("close", async () => {
       const log = `[ ${req.method} ]${req.originalUrl} ${date} [ IP ]${req.ip} [ STATUS ]${res.statusCode}\n\n`;
       console.log(log);
-      enableOutput && (await fs.appendFile("log.txt", log));
+      if (enableOutput) {
+        const existentJson = await fs
+          .readFile("log.json", "utf-8")
+          .catch(() => {
+            return null;
+          });
+        if (!existentJson) {
+          await fs.writeFile(
+            "log.json",
+            JSON.stringify(
+              [
+                {
+                  method: req.method,
+                  url: req.originalUrl,
+                  date: date,
+                  ip: req.ip,
+                  status: res.statusCode,
+                },
+              ],
+              null,
+              2
+            )
+          );
+        } else {
+          const logJson = JSON.parse(existentJson);
+          logJson.push({
+            method: req.method,
+            url: req.originalUrl,
+            date: date,
+            ip: req.ip,
+            status: res.statusCode,
+          });
+          await fs.writeFile("log.json", JSON.stringify(logJson, null, 2));
+        }
+      }
+
       next();
     });
     next();
